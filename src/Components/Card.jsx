131aea5f8206aed5a72b66f05307ec3f5771506a
ozlemezgi <ozlemezgisari@icloud.com>
2023-04-24 23:15:35 +0300

    card component complated

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -4,14 +4,21 @@ function Card({vehicle}) {
   console.log(vehicle);
   return (
     <>
-      <div className="card">
-        <img src="./images/starship.avif" alt="" />
-        <div className="bottom">
-          <h3 className="title">Vehicle Name</h3>
-          <p className="model">Model:Model</p>
-          <p className="rating">Hyperdrive Raiting : Hyperdrive Raiting</p>
-        </div>
-      </div>
+    {
+      vehicle.map((item)=>{
+        return (
+          <div className="card">
+            <img src="./images/starship.avif" alt="" />
+            <div className="bottom">
+              <h3 className="title">{item.name}</h3>
+              <p className="model">Model:{item.model}</p>
+              <p className="rating">Hyperdrive Rating : {item.hyperdrive_rating}</p>
+            </div>
+          </div>
+        );
+      })
+    }
+    
     </>
   );
 }
