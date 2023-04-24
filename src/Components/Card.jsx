1f5f9448eb4e75fc43e6f7b5087d0b2d4d8e537d
ozlemezgi <ozlemezgisari@icloud.com>
2023-04-25 00:14:40 +0300

    detail component complated

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,20 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Detail from './Detail';
 
 function Card({vehicle}) {
   console.log(vehicle);
+
+  const [show,setShow]=useState(false);
+  const [vehicleItem,setVehicleItem] =useState();
   return (
     <>
     {
       vehicle.map((item)=>{
         return (
-          <div className="card">
-            <img src="./images/starship.avif" alt="" />
-            <div className="bottom">
-              <h3 className="title">{item.name}</h3>
-              <p className="model">Model:{item.model}</p>
-              <p className="rating">Hyperdrive Rating : {item.hyperdrive_rating}</p>
+          <>
+            <div className="card" onClick={()=>{setShow
+            (true);setVehicleItem(item)}}>
+              <img src="./images/starship.avif" alt="" />
+              <div className="bottom">
+                <h3 className="title">{item.name}</h3>
+                <p className="model">Model:{item.model}</p>
+                <p className="rating">
+                  Hyperdrive Rating : {item.hyperdrive_rating}
+                </p>
+              </div>
             </div>
-          </div>
+            <Detail show={show} item={vehicleItem} onClose={()=>setShow(false)}/>
+          </>
         );
       })
     }
---
+++ src/Components/Detail.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import {AiOutlineClose} from "react-icons/ai"
+
+function Detail({show,item,onClose}) {
+    if(!show){
+        return null;
+    }
+  return (
+    <>
+      <div className="overlay">
+        <div className="overlay-inner">
+          <button className="close" onClick={onClose}>
+            <AiOutlineClose />
+          </button>
+          <div className="inner-box">
+            <h2>{item.name}</h2>
+            <img src="./images/starship.avif" alt="" />
+            <div className="info">
+              <h4>
+                Model<span>{item.model}</span>
+              </h4>
+              <br />
+              <h4>
+                Hyperdrive Rating<span>{item.hyperdrive_rating}</span>
+              </h4>
+              <br />
+              <h4>
+                Passengers<span>{item.passengers}</span>
+              </h4>
+              <br />
+              <h4>
+                Max Atmosphering Speed<span>{item.max_atmosphering_speed}</span>
+              </h4>
+              <br />
+              <h4>
+                Manufacturer<span>{item.manufacturer}</span>
+              </h4>
+              <br />
+              <h4>
+                Crew<span>{item.crew}</span>
+              </h4>
+              <br />
+              <h4>
+                Cargo Capacity<span>{item.cargo_capacity}</span>
+              </h4>
+              <br />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Detail
